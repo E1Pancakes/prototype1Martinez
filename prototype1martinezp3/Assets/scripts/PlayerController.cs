1d1a16dcfa60ac6d79ea92ebd17fe190adedac16
Diego Martinez <718717@student.dusd.net>
2024-03-06 11:42:11 -0800

    im done now just doin the challenge

--- prototype1martinezp3/Assets/scripts/PlayerController.cs
+++ prototype1martinezp3/Assets/scripts/PlayerController.cs
@@ -14,6 +14,9 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
 
     void Update()
     {
@@ -23,5 +26,10 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled; 
+        }
     }
 }
